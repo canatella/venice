5b5b8bbb25bad2c68e5ff73c9d8e7d60af791f6f
Damien Merenne <dam@cosinux.org>
2016-10-18 11:30:57 +0200

    Latest receipt info is not an array anymore.

--- lib/venice/client.rb
+++ lib/venice/client.rb
@@ -46,9 +46,15 @@ module Venice
         if latest_receipt_info_attributes = json['latest_receipt_info']
           # AppStore returns 'latest_receipt_info' even if we use over iOS 6. Besides, its format is an Array.
           receipt.latest_receipt_info = []
-          latest_receipt_info_attributes.each do |latest_receipt_info_attribute|
-            # latest_receipt_info format is identical with in_app
-            receipt.latest_receipt_info << InAppReceipt.new(latest_receipt_info_attribute)
+          if latest_receipt_info_attributes.is_a?(Array)
+            latest_receipt_info_attributes.each do |latest_receipt_info_attribute|
+              # latest_receipt_info format is identical with in_app
+              receipt.latest_receipt_info <<
+                InAppReceipt.new(latest_receipt_info_attribute)
+            end
+          else
+            receipt.latest_receipt_info <<
+              InAppReceipt.new(latest_receipt_info_attributes)
           end
         end
 
@@ -78,7 +84,6 @@ module Venice
       request.body = parameters.to_json
 
       response = http.request(request)
-
       JSON.parse(response.body)
     end
   end
